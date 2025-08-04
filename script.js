d26d88819486944fa99463d9cc0aabc1fbdd944c
jorgeeshun <cl>
2025-08-04 19:17:02 +0000

    Add Task modified

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create a new button to remove the task
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
-        removeBtn.className.add = 'remove-btn';
+        removeBtn.classList.add = 'remove-btn';
 
         // Add an event listener to the remove button
         removeBtn.onclick = function() {
